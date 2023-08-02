ce537cfda9d999d405dbac06c5795ff9d6c1487b
rahul <rkamat02@students.cs.ubc.ca>
2023-08-02 13:44:56 -0700

    implemented load action

--- src/main/ui/InventoryUI.java
+++ src/main/ui/InventoryUI.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.InvalidPathException;
 
 public class InventoryUI extends JFrame {
     private static final String JSON_LOC = "./data/I||nventory.json";
@@ -55,11 +57,32 @@ public class InventoryUI extends JFrame {
         buttons.add(new JButton("View Items to Restock"));
         buttons.add(new JButton("Set Discount"));
         buttons.add(new JButton("View Sales"));
-        buttons.add(new JButton("Load Inventory"));
+        buttons.add(new JButton(new LoadAction()));
         buttons.add(new JButton(new SaveAction()));
         buttons.add(new JButton(new QuitAction()));
 
         menuPane.add(buttons, BorderLayout.WEST);
+    }
+
+    private class LoadAction extends AbstractAction {
+
+
+        LoadAction() {
+            super("Load Save");
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            try {
+                inv = jsonReader.read();
+            } catch (IOException er) {
+                String msg = "File Not Accessible: " + JSON_LOC;
+                JOptionPane.showMessageDialog(null, msg, "System Error", JOptionPane.INFORMATION_MESSAGE);
+            } catch (InvalidPathException er) {
+                String msg = "Invalid Path: " + JSON_LOC;
+                JOptionPane.showMessageDialog(null, msg, "System Error", JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
     }
 
     private class SaveAction extends AbstractAction {
@@ -76,8 +99,8 @@ public class InventoryUI extends JFrame {
                 jsonWriter.write(inv);
                 jsonWriter.close();
             } catch (FileNotFoundException er) {
-                JOptionPane.showMessageDialog(null, "System Error: File Not Found"
-                );
+                String msg = "File Not Found: " + JSON_LOC;
+                JOptionPane.showMessageDialog(null, msg, "System Error", JOptionPane.INFORMATION_MESSAGE);
             }
         }
     }
