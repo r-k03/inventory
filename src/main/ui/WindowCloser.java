1091896bef8c4b6b10c1e3937961c2229142b74e
rahul <rkamat02@students.cs.ubc.ca>
2023-08-09 00:23:48 -0700

    added specification for WindowCloser

--- src/main/ui/WindowCloser.java
+++ src/main/ui/WindowCloser.java
@@ -7,12 +7,16 @@ import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.awt.event.WindowStateListener;
 
+// represents a window listener
 public class WindowCloser implements WindowListener {
+
+    // EFFECTS: nothing
     @Override
     public void windowOpened(WindowEvent e) {
 
     }
 
+    // EFFECTS: prints the event log and exits the application
     @Override
     public void windowClosing(WindowEvent e) {
         for (Event next : EventLog.getInstance()) {
@@ -21,26 +25,31 @@ public class WindowCloser implements WindowListener {
         System.exit(0);
     }
 
+    // EFFECTS: nothing
     @Override
     public void windowClosed(WindowEvent e) {
 
     }
 
+    // EFFECTS: nothing
     @Override
     public void windowIconified(WindowEvent e) {
 
     }
 
+    // EFFECTS: nothing
     @Override
     public void windowDeiconified(WindowEvent e) {
 
     }
 
+    // EFFECTS: nothing
     @Override
     public void windowActivated(WindowEvent e) {
 
     }
 
+    // EFFECTS: nothing
     @Override
     public void windowDeactivated(WindowEvent e) {
 
